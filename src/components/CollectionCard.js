8b85618439e31865ba05daa8a2507efd4f5cd16f
lance-malaga <lmalaga@my.bcit.ca>
2023-10-24 02:45:59 -0700

    update your garden page and component

---
+++ src/components/CollectionCard.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { useEffect, useState } from 'react';
+import { StyleSheet, View, Image } from "react-native";
+import CustomText from "./CustomText";
+
+export default function CollectionCard({name, status, image}) {
+    return (
+        <View style={styles.container}>
+            <View style={styles.img_container}>
+                <Image source={image} alt={name} style={styles.plant_img} />
+            </View>
+            <View style={styles.plant_text}>
+                <CustomText
+                    text={name}
+                    fontSize={18}
+                    fontWeight={500}
+                />
+                <CustomText
+                    text={status}
+                />
+            </View>
+            <Image source={require('../../assets/icons/3.0_arrow.png')} alt={name} style={styles.arrow} />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        paddingHorizontal: 15,
+        paddingVertical: 5,
+        alignItems: "center",
+        backgroundColor: "white",
+        borderRadius: 16,
+        marginLeft: -40,
+    },
+    img_container: {
+        width: 95,
+        height: 105,
+        position: "relative",
+        justifyContent: "center",
+        marginLeft: 15,
+    },
+    plant_img: {
+        position: "absolute",
+    },
+    arrow: {
+        marginTop: "auto",
+    }
+});
--- src/screens/YourGarden.js
+++ src/screens/YourGarden.js
@@ -1,13 +1,163 @@
 import React from "react";
 import { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, ScrollView, Image, Pressable, SafeAreaView, FlatList } from "react-native";
+import { StyleSheet, Text, View, ScrollView, Image, Pressable, SafeAreaView, FlatList, Dimensions } from "react-native";
+
+// COMPONENTS
 import NavBar from "../components/NavBar";
+import Header from "../components/Header";
+import GlobalStyles from "../components/GlobalStyles";
+import CustomText from "../components/CustomText";
+import SearchBar from "../components/SearchBar";
+import CollectionCard from "../components/CollectionCard";
+
+const windowHeight = Dimensions.get('window').height;
 
 export default function YourGarden() {
+    const filterList = [ 'All', 'Vegetable', 'Fruites', 'Herbs', 'Legumes', 'Flowers' ];
+    const taskList = [
+        {
+            name: 'Tomato Plant',
+            status: 'Your plant is doing great!',
+            image: require('../../assets/images/3.0_tomato.png'),
+        },
+        {
+            name: 'Cucumber Plant',
+            status: 'Your plant is doing okay',
+            image: require('../../assets/images/3.0_cucumber.png'),
+        },
+        {
+            name: 'Squash',
+            status: 'Your plant is doing okay',
+            image: require('../../assets/images/3.0_squash.png'),
+        },
+    ];
+
     return (
         <>
-            <Text>Your Garden</Text>
-            <NavBar/>
+            <SafeAreaView contentContainerStyle={GlobalStyles.androidSafeArea}>
+                <View style={styles.container}>
+                    <Header />
+                    <Image 
+                        source={require('../../assets/images/3.0_bg_img.png')}
+                        alt="bg-img"
+                        style={styles.bg_img}
+                    />
+                    <ScrollView>
+                        <View style={styles.greeting}>
+                            <View style={styles.heading_container}>
+                                <Text style={[styles.heading_text]}>Hi </Text>
+                                <Text style={[styles.heading_text, styles.heading_name]}>Jimmy!</Text>
+                            </View>
+                            <Text style={styles.text}>How are your plants today?</Text>
+                        </View>
+                        <View style={styles.your_garden_content}>
+                            <SearchBar />
+                            <View style={styles.filter_header}>
+                                    <CustomText
+                                        text={`My Garden`}
+                                        color={'white'}
+                                        fontSize={18}
+                                        fontWeight={700}
+                                    />
+                                    <Pressable>
+                                        <Text style={styles.button}>ADD PLANT</Text>
+                                    </Pressable>
+                            </View>
+                            <ScrollView horizontal>
+                                <View style={styles.filter_content}>
+                                    {filterList.map((a, index) => {
+                                        return (
+                                                <Pressable key={index}>
+                                                    <CustomText 
+                                                        text={a}
+                                                        fontSize={16}
+                                                        fontWeight={500}
+                                                        color={a === 'All' ? 'white' : '#50B7AD'}
+                                                    />
+                                                </Pressable>
+                                        )
+                                    })}
+                                </View>
+                            </ScrollView>
+                            <View style={styles.collection_container}>
+                                {taskList.map((a, index) => {
+                                    return (
+                                        <CollectionCard 
+                                        key={index}
+                                        name={a.name}
+                                        status={a.status}
+                                        image={a.image}
+                                        />
+                                    );
+                                })}
+                            </View>
+                        </View>
+                    </ScrollView>
+                    <NavBar />
+                </View>
+            </SafeAreaView>
         </>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        height: windowHeight,
+        // backgroundColor: "#FAFAFA",
+        // backgroundColor: "blue",
+    },
+    greeting: {
+        marginTop: 10,
+        paddingHorizontal: 24,
+        gap: 5,
+        // backgroundColor: "red",
+    },
+    heading_container: {
+        flexDirection: "row",
+        gap: 3,
+    },
+    heading_text: {
+        fontSize: 24,
+        color: "white",
+    },
+    heading_name: {
+        fontWeight: 700
+    },
+    text: {
+        fontSize: 16,
+        color: "white",
+    },
+
+    your_garden_content: {
+        paddingBottom: 150,
+        paddingHorizontal: 24,
+        // width: "100%",
+    },
+    bg_img: {
+        position: "absolute",
+        top: 0,
+        zIndex: -1,
+        width: "100%",
+        // marginTop: 30,
+    },
+    filter_header: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        marginBottom: 10,
+        marginTop: 55,
+    },
+    button: {
+        borderBottomColor: "white",
+        borderBottomWidth: 1,
+        color: "white",
+    },
+    filter_content: {
+        flexDirection: "row",
+        gap: 25,
+    },
+    collection_container: {
+        marginTop: 40,
+        gap: 25,
+    },
+});
