a86069ea7cb8191458a77dd2387017a701649901
Andrea Wu <andrea.wzg@gmail.com>
2023-11-16 23:30:39 -0800

    add detail-info for 'Plant Detail' page

--- src/screens/PlantDetail.js
+++ src/screens/PlantDetail.js
@@ -48,6 +48,7 @@ export default function PlantDetail({route}) {
                         <Image source={require("../../assets/icons/backIcon.png")} alt="back-icon" />
                     </TouchableOpacity>
                 </View>
+
                 <FontText
                     content={chosenPlant.name}
                     fontSize={18}
